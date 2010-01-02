0bbd4f998c3c74f4a84f05ff4527bcc1cc29e072
jbbarth <jeanbaptiste.barth@gmail.com>
2010-01-02 18:35:20 +0100

    Initialized empty plugin

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_drafts do
+  name 'Redmine Drafts plugin'
+  description 'This plugin avoids losing data when editing issues by saving it regularly as a draft'
+  version '0.0.1'
+  url 'http://github.com/jbbarth/redmine_drafts'
+  author 'Jean-Baptiste BARTH'
+  author_url 'mailto:jeanbaptiste.barth@gmail.com'
+  version '0.1'
+end
