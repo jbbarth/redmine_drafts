dc11d273e17673358efde2c470f7b716970092d0
jbbarth <jeanbaptiste.barth@gmail.com>
2010-01-04 19:43:08 +0100

    Fixed oopsie in drafts_controller

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -7,7 +7,7 @@ class DraftsController < ApplicationController
       lock_version = params[:issue][:lock_version].to_i
       @user = User.find(params[:user_id])
       @draft = Draft.find_for_issue(@issue,@user,lock_version)
-      @draft ||= Draft.create(:user => @user, :element => @issue, lock_version)
+      @draft ||= Draft.create(:user => @user, :element => @issue, :lock_version => lock_version)
       @draft.content = params.reject{|k,v| !%w(issue notes).include?(k)}
       if @draft.save
         render :partial => "saved", :layout => false
