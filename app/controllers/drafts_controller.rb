d031301bdba9ebfc025c58966568861d017a6ddd
jbbarth <jeanbaptiste.barth@gmail.com>
2010-01-06 09:30:58 +0100

    Fixed: 'lock_version' was not a good idea since it's a magical column in active_record, incremented automatically ; renamed it so that drafts are saved with the good, current lock_version of the issue

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -2,16 +2,19 @@ class DraftsController < ApplicationController
   unloadable
 
   def create
-    if request.xhr? && !params[:issue][:notes].blank?
+    if request.xhr? && !params[:notes].blank?
       @issue = Issue.find(params[:issue_id])
-      lock_version = params[:issue][:lock_version].to_i
+      version = params[:issue][:lock_version].to_i
       @user = User.find(params[:user_id])
-      @draft = Draft.find_for_issue(@issue,@user,lock_version)
-      @draft ||= Draft.create(:user => @user, :element => @issue, :lock_version => lock_version)
-      @draft.content = params.reject{|k,v| !%w(issue notes).include?(k)}
+      @draft = Draft.find_for_issue(@issue,@user,version)
+      @draft ||= Draft.create(:user => @user, :element => @issue, :element_lock_version => version)
       if @draft.save
         render :partial => "saved", :layout => false
+      else
+        render :text => "Error saving draft"
       end
+    else
+      render :nothing => true
     end
   end
 end
