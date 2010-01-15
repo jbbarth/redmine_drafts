367b97be5e7aef5c7e31d0c2054ff5c46d3c3c9a
jbbarth <jeanbaptiste.barth@gmail.com>
2010-01-15 17:20:24 +0100

    Let's say current version is 0.0.2 (and not 'both 0.0.1 and 0.1' ...)

--- init.rb
+++ init.rb
@@ -9,9 +9,8 @@ end
 Redmine::Plugin.register :redmine_drafts do
   name 'Redmine Drafts plugin'
   description 'This plugin avoids losing data when editing issues by saving it regularly as a draft'
-  version '0.0.1'
+  version '0.0.2'
   url 'http://github.com/jbbarth/redmine_drafts'
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  version '0.1'
 end
