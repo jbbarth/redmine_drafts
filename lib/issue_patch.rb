4232c3ab88b427aab5c6bcb42fbb06f275a952ad
jbbarth <jeanbaptiste.barth@gmail.com>
2010-01-15 13:46:14 +0100

    Added Issue#after_save callbacks to clean drafts when issue is saved

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -2,4 +2,16 @@ require_dependency 'issue'
 
 class Issue
   has_many :drafts, :as => :element
+
+  after_create :clean_drafts_after_create
+  after_update :clean_drafts_after_update
+  
+  def clean_drafts_after_create
+    draft = Draft.find_for_issue(:element_id => 0, :user_id => User.current.id)
+    draft.destroy if draft
+  end
+  
+  def clean_drafts_after_update
+    self.drafts.select{|d| d.user_id == User.current.id}.each(&:destroy)
+  end
 end
---
+++ test/unit/issue_drafts_test.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class IssueDraftsTest < ActiveSupport::TestCase
+  fixtures :users, :issues
+
+  def test_clean_drafts_on_create
+    User.current = User.find(3)
+    issue = Issue.new(:project_id => 1, :tracker_id => 1, :author_id => 3, :status_id => 1, :priority => IssuePriority.all.first, :subject => 'test_create', :description => 'IssueTest#test_create')
+    conds = {:user_id => 3, :element_id => 0}
+    assert Draft.find_or_create_for_issue(conds).is_a?(Draft)
+    assert_not_nil Draft.find_for_issue(conds)
+    assert issue.save
+    assert_nil Draft.find_for_issue(conds)
+  end
+
+  def test_clean_drafts_on_update
+    issue = Issue.find(1)
+    User.current = issue.author
+    conds = {:user_id => issue.author.id, :element_id => issue.id}
+    assert Draft.find_or_create_for_issue(conds.merge(:element_lock_version => issue.lock_version)).is_a?(Draft)
+    assert_not_nil Draft.find_for_issue(conds)
+    issue.subject = "Another wonderful subject"
+    issue.save
+    assert_nil Draft.find_for_issue(conds)
+  end
+end
