f40a06d2d891e6f165f6bfdffcbcfded7b7d5420
jbbarth <jeanbaptiste.barth@gmail.com>
2010-05-14 15:41:33 +0200

    Set version to 0.1.1

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ end
 Redmine::Plugin.register :redmine_drafts do
   name 'Redmine Drafts plugin'
   description 'This plugin avoids losing data when editing issues by saving it regularly as a draft'
-  version '0.1'
+  version '0.1.1'
   url 'http://github.com/jbbarth/redmine_drafts'
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
