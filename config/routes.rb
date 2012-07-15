fcaf040656ef799ed9072a7da05caccdf115f568
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-15 19:19:52 +0200

    Fixed bad route for /drafts/autosave

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RedmineApp::Application.routes.draw do
   resources :drafts, :only => [:create, :destroy] do
-    collection { post :autosave }
+    collection { put :autosave }
     member { put :restore }
   end
 end
