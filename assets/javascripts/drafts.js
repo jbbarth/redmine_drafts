5ecbc907ac706231b1d2d71d628b98c0d9d6fab2
sk-ys <yskrmt@gmail.com>
2025-02-24 02:29:40 +0000

    Add support for draft attachments restoration

---
+++ assets/javascripts/drafts.js
@@ -0,0 +1,33 @@
+function restoreDraftFile(inputEl, fileName, attachmentDescription, attachmentToken, attachmentPath) {
+// Base on the original addFile function
+const attachmentsFields = $(inputEl).closest('.attachments_form').find('.attachments_fields');
+const addAttachment = $(inputEl).closest('.attachments_form').find('.add_attachment');
+const maxFiles = ($(inputEl).attr('multiple') == 'multiple' ? 10 : 1);
+
+const attachmentId = restoreDraftFile.nextAttachmentId++;
+addFile.nextAttachmentId = attachmentId;
+
+const fileSpan = $('<span>', { id: 'attachments_' + attachmentId });
+let param = $(inputEl).data('param');
+if (!param) {param = 'attachments'};
+
+fileSpan.append(
+    $('<input>', { type: 'text', 'class': 'icon icon-attachment filename readonly', name: param +'[' + attachmentId + '][filename]', readonly: 'readonly'} ).val(fileName),
+    $('<input>', { type: 'text', 'class': 'description', name: param + '[' + attachmentId + '][description]', maxlength: 255, placeholder: $(inputEl).data('description-placeholder') } ).val(attachmentDescription),
+    $('<input>', { type: 'hidden', 'class': 'token', name: param + '[' + attachmentId + '][token]'} ).val(attachmentToken),
+    $('<a>&nbsp</a>').attr({ href: "#", 'class': 'icon-only icon-del remove-upload' }).click(removeFile).attr({
+        "data-remote": true,
+        "data-method": 'delete',
+        href: attachmentPath
+        })
+        .off('click')
+).appendTo(attachmentsFields);
+
+if ($(inputEl).data('description') == 0) {
+fileSpan.find('input.description').remove();
+}
+
+addAttachment.toggle(attachmentsFields.children().length < maxFiles);
+return attachmentId;
+}
+restoreDraftFile.nextAttachmentId = 1;
--- lib/redmine_drafts/issues_controller_patch.rb
+++ lib/redmine_drafts/issues_controller_patch.rb
@@ -5,9 +5,34 @@ module RedmineDrafts::IssuesControllerPatch
     if params[:draft_id].present?
       draft = Draft.find(params[:draft_id]) rescue nil
       if draft.present?
-        params.merge!(draft.content.permit!)
+        @draft_attachments = prepare_draft_attachments(draft.content["attachments"].permit!.to_h)
+        params.merge!(draft.content.reject{|k,v| k == "attachments"}.permit!)
       end
     end
+  end
+
+  private
+
+  def prepare_draft_attachments(draft_params)
+    return [] unless draft_params.present?
+
+    attachments = draft_params.map do |_, param|
+      token = param['token']
+      attachment_id = token.split('.').first
+      attachment = Attachment.find_by(id: attachment_id)
+
+      next nil unless attachment
+
+      {
+        filename: attachment.filename,
+        description: attachment.description,
+        token: token,
+        path: attachment_path(attachment, format: 'js')
+      }
+    end
+
+    @failed_draft_attachments_count = attachments.count(nil)
+    attachments.compact
   end
 end
 
@@ -18,4 +43,3 @@ class IssuesController
   prepend_before_action :set_draft, :only => [:new, :edit]
 
 end
-
