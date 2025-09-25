a9516384aa248539adf40ccd82ea038eaf4c8a4b
Vincent <contact@vincent-robert.com>
2025-09-25 08:11:42 +0200

    Remove obsolete CI workflow files

--- db/migrate/20250701160618_add_missing_indexes.rb
+++ db/migrate/20250701160618_add_missing_indexes.rb
@@ -1,4 +1,4 @@
-class AddMissingIndexes < ActiveRecord::Migration[7.2]
+class AddMissingIndexes < ActiveRecord::Migration[6.1]
   def change
     add_index :drafts, [:element_id, :element_type], if_not_exists: true
   end
